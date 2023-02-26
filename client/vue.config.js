d7c2b338d5d41a1881c189177eb0d3afa1f4c77b
handmadeCode <sbae5617@gmail.com>
2023-02-27 00:29:54 +0900

    Vuetify 설정

--- client/vue.config.js
+++
@@ -1,8 +0,0 @@
-const { defineConfig } = require("@vue/cli-service");
-module.exports = defineConfig({
-    transpileDependencies: true,
-    outputDir: "../movie/src/main/resources/static",
-    devServer: {
-        proxy: "http://localhost:8080",
-    },
-});
