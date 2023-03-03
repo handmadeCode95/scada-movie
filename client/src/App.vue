e99236293cf54c5a3d3b9e10118b5aa665146f0a
handmadeCode <sbae5617@gmail.com>
2023-03-03 23:10:06 +0900

    Vuetify 재세팅

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,7 +1,15 @@
 <template>
-    <router-view />
+    <v-app>
+        <v-main>
+            <TheLayoutVue></TheLayoutVue>
+        </v-main>
+    </v-app>
 </template>
 
 <script setup>
-//
+import TheLayoutVue from "./layout/TheLayout.vue";
+
+components: {
+    TheLayoutVue;
+}
 </script>
